f326ce41c95efd7cee7c18bfa02f5f85bd6901fa
ad03140581 <jihwan0_0kim@linecorp.com>
2025-07-18 01:11:44 +0900

    chore: update config, dependencies, and types for new features

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,3 +1,4 @@
+/// <reference types="@sveltejs/kit" />
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
 declare global {
@@ -11,3 +12,12 @@ declare global {
 }
 
 export {};
+
+declare module 'flowbite-svelte-admin-dashboard/AppsMenu.svelte' {
+  import { SvelteComponentTyped } from 'svelte';
+  export default class AppsMenu extends SvelteComponentTyped<any, any, any> {}
+}
+declare module 'flowbite-svelte-admin-dashboard/CardWidget.svelte' {
+  import { SvelteComponentTyped } from 'svelte';
+  export default class CardWidget extends SvelteComponentTyped<any, any, any> {}
+}
